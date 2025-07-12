d934d13daffe372875a2b015ca5a3e8e483313fb
Ashwin <krias@utschools.ca>
2025-07-12 18:20:48 -0400

    Test Vercel deploy

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Navbar = () => (
+  <nav className="w-full p-4 bg-blue-700 text-white font-bold text-xl">
+    Jaffna Hindu Ladies College
+  </nav>
+);
+
+export default Navbar;
